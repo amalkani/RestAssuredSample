44c50824de35397fcbf9be7a4b0bd7ce0398e3e1
Pushkala Pattabhiraman <pushkala.pattabhiraman@zuora.com>
2018-01-16 23:22:22 -0800

    Adding few more fixes

--- src/test/java/com/api/tests/FeatureFlagTests.java
+++ src/test/java/com/api/tests/FeatureFlagTests.java
@@ -31,6 +31,40 @@ public class FeatureFlagTests extends ApiTestsBase {
 		// NEED API TO CLEAN UP ENVIRONMENTS CREATED BY EACH TEST
 	}
 
+	/**
+	 * Assumption here is that there are no pre-existing Environments
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void newDefaultFeatureTest() throws Exception {
+		// Create a new Feature
+		FeatureFlag defaultFeature = new FeatureFlag("StandandFeature1", false, null);
+		ArrayList<FeatureFlag> featureFlags = new ArrayList<FeatureFlag>();
+		featureFlags.add(defaultFeature);
+
+		// Create a new Environment
+		Environment env = new Environment("newDefaultFeature", "newDefaultFeature", featureFlags);
+		Environment new_env = environmentHelper.createEnvironment(env);
+		assertThat(new_env).isNotNull();
+
+		// Retrive List of Environments and compare the environment with the original
+		// request
+		Environment[] environments = environmentHelper.listAllEnvironments();
+
+		assertThat(environments).describedAs("Verify that one environment has been created").hasSize(1);
+		assertThat(environments[0].getName()).describedAs("Verify the environment created matches request")
+				.isEqualTo("newDefaultFeature");
+
+		assertThat(environments[0].getFeatureFlags()).describedAs("Verify there are default flags").hasSize(1);
+
+		assertThat(environments[0].getFeatureFlags().get(0).getFeatureflagName())
+				.describedAs("Verify Feature Flag name matches request").isEqualTo("StandandFeature1");
+
+		assertThat(environments[0].getFeatureFlags().get(0).getFeatureStatus())
+				.describedAs("Verify Standard OOB Feature appears disabled").isFalse();
+	}
+
 	@Test
 	// Creating a feature flag that is to be shared by sub-teams
 	public void newSharedFeatureTest() throws Exception {
@@ -72,15 +106,79 @@ public class FeatureFlagTests extends ApiTestsBase {
 	}
 
 	@Test
-	public void newEnvironmentTest() throws Exception {
-	}
+	public void newFeatureTest_toggleState() throws Exception {
+		FeatureFlag defaultFeature = new FeatureFlag("DefaultFeature1", false, null);
+		ArrayList<FeatureFlag> featureFlags = new ArrayList<FeatureFlag>();
+		featureFlags.add(defaultFeature);
 
-	@Test
-	public void newFeatureTest() throws Exception {
+		// Create a new Environment
+		Environment env = new Environment("newFeatureTest_false", "newFeatureTest_false", featureFlags);
+		Environment new_env = environmentHelper.createEnvironment(env);
+		assertThat(new_env).isNotNull();
+
+		FeatureFlag envFeature = featureFlagHelper.getFeatureFlag(new_env, defaultFeature);
+		assertThat(envFeature).isNotNull();
+		assertThat(envFeature.getFeatureStatus()).describedAs("Verifying the Default status is False").isFalse();
+
+		envFeature.setFeatureEnabled(true);
+		featureFlagHelper.updateFlag(new_env, envFeature);
+
+		envFeature = featureFlagHelper.getFeatureFlag(new_env, defaultFeature);
+		assertThat(envFeature).isNotNull();
+		assertThat(envFeature.getFeatureStatus()).describedAs("Updated Status is True").isTrue();
+
+		envFeature.setFeatureEnabled(false);
+		featureFlagHelper.updateFlag(new_env, envFeature);
+
+		envFeature = featureFlagHelper.getFeatureFlag(new_env, defaultFeature);
+		assertThat(envFeature).isNotNull();
+		assertThat(envFeature.getFeatureStatus()).describedAs("Verifying the Default status is False").isFalse();
+
 	}
 
 	@Test
 	public void newCrossTeamFeatureTest() throws Exception {
+		FeatureFlag parentFeature = new FeatureFlag("SharedFeature1", true, null);
+		FeatureFlag teamFeature1 = new FeatureFlag("Team1", false, "SharedFeature1");
+		FeatureFlag teamFeature2 = new FeatureFlag("Team2", false, "SharedFeature1");
+		ArrayList<FeatureFlag> featureFlags = new ArrayList<FeatureFlag>();
+		featureFlags.add(parentFeature);
+		featureFlags.add(teamFeature1);
+		featureFlags.add(teamFeature2);
+
+		// Create a new Environment
+		Environment env = new Environment("newSharedFeatureTest", "newDefaultFeature", featureFlags);
+		Environment new_env = environmentHelper.createEnvironment(env);
+		assertThat(new_env).isNotNull();
+
+		// Retrieve the environment and compare the flags
+		Environment environment = environmentHelper.describeEnvironment(new_env);
+		assertThat(environment.getName()).describedAs("Verify the environment created matches request")
+				.isEqualTo("newSharedFeatureTest");
+		ArrayList<FeatureFlag> features = featureFlagHelper.getListOfFeaturesInHeirarchy(environment, "SharedFeature1");
+		
+		FeatureFlag envFeature = featureFlagHelper.getFeatureFlag(new_env, teamFeature1);
+		assertThat(envFeature.getFeatureStatus()).describedAs("Default Status is False").isFalse();
+		
+	
+		FeatureFlag envFeature2 = featureFlagHelper.getFeatureFlag(new_env, teamFeature2);
+		assertThat(envFeature.getFeatureStatus()).describedAs("Default Status is False").isFalse();
+         
+		envFeature.setFeatureEnabled(false);
+		featureFlagHelper.updateFlag(new_env, envFeature);
+		 envFeature = featureFlagHelper.getFeatureFlag(new_env, teamFeature1);
+		assertThat(envFeature.getFeatureStatus()).describedAs("Updated Status is True").isTrue();
+		envFeature2 = featureFlagHelper.getFeatureFlag(new_env, teamFeature2);
+		assertThat(envFeature2.getFeatureStatus()).describedAs("Default Status is False").isFalse();
+		envFeature2.setFeatureEnabled(true);
+		featureFlagHelper.updateFlag(new_env, envFeature2);
+		 envFeature = featureFlagHelper.getFeatureFlag(new_env, teamFeature1);
+		assertThat(envFeature.getFeatureStatus()).describedAs("Updated Status is True").isTrue();
+		
+		envFeature2 = featureFlagHelper.getFeatureFlag(new_env, teamFeature2);
+		assertThat(envFeature2.getFeatureStatus()).describedAs("Updated Status is True").isFalse();
+		
+		
 	}
 
 	@Test
@@ -117,40 +215,6 @@ public class FeatureFlagTests extends ApiTestsBase {
 
 	@Test
 	public void getAllFlags_MultipleHeirarchyTest() throws Exception {
-	}
-
-	/**
-	 * Assumption here is that there are no pre-existing Environments
-	 * 
-	 * @throws Exception
-	 */
-	@Test
-	public void newDefaultFeatureTest() throws Exception {
-		// Create a new Feature
-		FeatureFlag defaultFeature = new FeatureFlag("StandandFeature1", false, null);
-		ArrayList<FeatureFlag> featureFlags = new ArrayList<FeatureFlag>();
-		featureFlags.add(defaultFeature);
-
-		// Create a new Environment
-		Environment env = new Environment("newDefaultFeature", "newDefaultFeature", featureFlags);
-		Environment new_env = environmentHelper.createEnvironment(env);
-		assertThat(new_env).isNotNull();
-
-		// Retrive List of Environments and compare the environment with the original
-		// request
-		Environment[] environments = environmentHelper.listAllEnvironments();
-
-		assertThat(environments).describedAs("Verify that one environment has been created").hasSize(1);
-		assertThat(environments[0].getName()).describedAs("Verify the environment created matches request")
-				.isEqualTo("newDefaultFeature");
-
-		assertThat(environments[0].getFeatureFlags()).describedAs("Verify there are default flags").hasSize(1);
-
-		assertThat(environments[0].getFeatureFlags().get(0).getFeatureflagName())
-				.describedAs("Verify Feature Flag name matches request").isEqualTo("StandandFeature1");
-
-		assertThat(environments[0].getFeatureFlags().get(0).getFeatureStatus())
-				.describedAs("Verify Standard OOB Feature appears disabled").isFalse();
 	}
 
 }
